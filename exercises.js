784936d4d85fdf5b225a91db556a57fe439d6bb2
unknown <asiabermea@gmail.com>
2019-01-11 21:37:50 -1000

    canVote

--- exercises.js
+++ exercises.js
@@ -35,6 +35,14 @@ console.log('another test')
  * The function will return true if the number passed into the function is equal to or greater than Hawaii's voting age. Console.log your result.
 */
 
+function canVote(age){
+  if(age >= 18){
+    return true;
+  }else {
+    return false;
+  }
+}
+console.log(canVote(24))
 
 /*
  * #2
