314063dcf0f01f39381fa6982fabcba9f2d5b926
unknown <asiabermea@gmail.com>
2019-01-11 21:43:40 -1000

    password success

--- exercises.js
+++ exercises.js
@@ -56,6 +56,12 @@ console.log(canVote(24))
  * Console.log your result.
 */
 
+function login(password){
+  if(password === 'test1234'){
+    return 'Login Success!'
+  }
+}
+console.log(login('test1234'))
 
 /*
  * #3
